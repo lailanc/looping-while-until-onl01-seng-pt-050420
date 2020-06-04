a07011a3db56c56a310b006df57e62975122827f
Laila Campbell <lailacampbell96@gmail.com>
2020-06-04 19:20:14 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -11,3 +11,17 @@ end
 end
 
 
+
+
+# def using_while
+#   levitation_force =6
+  
+#   #your code here
+#   while levitation_force<10
+#   puts 'Wingardium Leviosa'
+  
+#   levitation_force += 1
+  
+# end
+  
+# end
