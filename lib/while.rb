806941efdc24215de44991a89df650e6870e9510
Laila Campbell <lailacampbell96@gmail.com>
2020-06-04 19:21:45 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,27 +1,27 @@
-def using_while
-  levitation_force = 6
-  
-# counter = 0 
-  while levitation_force < 10
-  puts 'Wingardium Leviosa'
-  levitation_force += 1  
-end 
-	
-  #your code here
-end
-
-
-
-
 # def using_while
-#   levitation_force =6
+#   levitation_force = 6
   
-#   #your code here
-#   while levitation_force<10
+# # counter = 0 
+#   while levitation_force < 10
 #   puts 'Wingardium Leviosa'
-  
-#   levitation_force += 1
-  
+#   levitation_force += 1  
+# end 
+	
+#   #your code here
 # end
+
+
+
+
+def using_while
+  levitation_force =6
   
-# end
+  #your code here
+  while levitation_force<10
+  puts 'Wingardium Leviosa'
+  
+  levitation_force += 1
+  
+end
+  
+end
